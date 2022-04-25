1b1583d9087c0d370987998a820e34480690faff
faizankhatik <khatikfaizan786@gmail.com>
2022-04-25 13:29:44 +0530

    ADDED:[faizan] programe for area of triangle

---
+++ TRIANGLE.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float area=0,base=4,height=9,half=0.5;
+	clrscr();
+	area=half*base*height;
+	printf("\Area of Triangle=%f",area);
+	getch();
+}
